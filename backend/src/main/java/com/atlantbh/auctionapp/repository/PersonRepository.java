d5fbfa4e6246b9999926f681128fdb2317831893
kkadusic <kkadusic1@etf.unsa.ba>
2021-02-26 04:36:47 +0100

    Add annotation for repository class, change ID type

--- backend/src/main/java/com/atlantbh/auctionapp/repository/PersonRepository.java
+++ backend/src/main/java/com/atlantbh/auctionapp/repository/PersonRepository.java
@@ -2,10 +2,10 @@ package com.atlantbh.auctionapp.repository;
 
 import com.atlantbh.auctionapp.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-public interface PersonRepository extends JpaRepository<Person, UUID> {
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
     boolean existsByEmail(String email);
 
