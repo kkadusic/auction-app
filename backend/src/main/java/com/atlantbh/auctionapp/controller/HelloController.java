8b4bf3067bb3353d11ab935ef44f44eb23c30ff7
kkadusic <kkadusic1@etf.unsa.ba>
2021-02-26 03:07:51 +0100

    Change package name

--- backend/src/main/java/com/atlantbh/auctionapp/controller/HelloController.java
+++ backend/src/main/java/com/atlantbh/auctionapp/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.atlantbh.auctionapp.controllers;
+package com.atlantbh.auctionapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
