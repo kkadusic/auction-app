b6fb48ff883f284b7e4af80820e54fd4ca7cd8bf
kkadusic <kkadusic1@etf.unsa.ba>
2021-03-02 00:19:14 +0100

    Change 404 not found page design

--- frontend/src/components/PageNotFound/index.js
+++ frontend/src/components/PageNotFound/index.js
@@ -1,10 +1,32 @@
+import {Button} from 'react-bootstrap';
+import {useHistory} from 'react-router-dom';
+import {RiAuctionFill} from "react-icons/ri";
+
+import './pageNotFound.css';
+
 const PageNotFound = () => {
+
+    const history = useHistory();
+
+    const goHome = () => {
+        history.push("/");
+    }
+
     return (
-        <div>
-            <img src="https://i.imgur.com/qIufhof.png" alt=""/>
-            <div>
-                <h3>This page could not be found</h3>
+        <div className="page-not-found-container">
+            <div className="page-not-found-header">
+                <RiAuctionFill className="auction-icon"/>
+                AUCTION
             </div>
+            <div className="page-not-found-404">
+                404
+            </div>
+            <div className="page-not-found-text">
+                Ooops! Looks like the page is Not Found
+            </div>
+            <Button onClick={goHome} className="btn-go-back"  variant="transparent-black-shadow">
+                GO BACK
+            </Button>
         </div>
     );
 }
