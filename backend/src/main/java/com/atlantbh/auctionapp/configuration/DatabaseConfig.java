bbf68e35304189a3991f1040d5c77b30f3060cc7
kkadusic <kkadusic1@etf.unsa.ba>
2021-04-07 17:42:51 +0200

    Update extensions.sql path

--- backend/src/main/java/com/atlantbh/auctionapp/configuration/DatabaseConfig.java
+++ backend/src/main/java/com/atlantbh/auctionapp/configuration/DatabaseConfig.java
@@ -15,7 +15,7 @@ public class DatabaseConfig {
     @Bean
     public DataSourceInitializer dataSourceInitializer(@Qualifier("dataSource") final DataSource dataSource) {
         ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-        resourceDatabasePopulator.addScript(new ClassPathResource("backend/src/main/resources/extensions.sql"));
+        resourceDatabasePopulator.addScript(new ClassPathResource("/extensions.sql"));
         DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
         dataSourceInitializer.setDataSource(dataSource);
         dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
