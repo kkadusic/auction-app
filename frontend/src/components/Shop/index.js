d8f1740aa43eb60c06063656cc771baa4261e698
kkadusic <kkadusic1@etf.unsa.ba>
2021-04-15 04:52:56 +0200

    Implement category/subcategory inclusive filter

--- frontend/src/components/Shop/index.js
+++ frontend/src/components/Shop/index.js
@@ -14,6 +14,8 @@ import {removeSpaces} from "../../utilities/AppUrl";
 import {useBreadcrumbContext, useAlertContext} from "../../AppContext";
 import ImageCardOverlay from "../ImageCardOverlay";
 import * as qs from 'query-string';
+import ReactTagInput from "@pathofdev/react-tag-input";
+import "@pathofdev/react-tag-input/build/index.css";
 
 import './shop.css';
 
@@ -32,6 +34,7 @@ const Shop = () => {
     const urlParams = qs.parse(history.location.search);
     const {setBreadcrumb} = useBreadcrumbContext();
     const {showMessage} = useAlertContext();
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
         setLoading(true);
@@ -69,6 +72,47 @@ const Shop = () => {
         refreshData(urlElements);
     }
 
+
+    const removeElement = (array, element) => {
+        const index = array.indexOf(element);
+        array.splice(index, 1);
+    }
+
+
+    const updateTags = (category, subcategory) => {
+        if (category != null && subcategory == null) {
+            let contains = false;
+            for (let t of tags) {
+                if (t[0] === category) {
+                    contains = true;
+                    removeElement(tags, t);
+                }
+            }
+            if (!contains) {
+                tags.push([category]);
+            }
+        } else if (category != null && subcategory != null) {
+            let contains = false;
+            for (let t of tags) {
+                if (getCategorySubcategoryFromTag(t[0]).subcategory === subcategory) {
+                    contains = true;
+                    removeElement(tags, t);
+                }
+            }
+            if (!contains) {
+                tags.push([category + ": " + subcategory]);
+            }
+        }
+    }
+
+    const getCategorySubcategoryFromTag = (tagName) => {
+        if (tagName.includes(":")) {
+            let list = tagName.split(": ");
+            return {category: list[0], subcategory: list[1]};
+        }
+        return {category: tagName, subcategory: null};
+    }
+
     const refreshData = async (urlElements) => {
         page = 0;
         const newFilter = {
@@ -78,10 +122,48 @@ const Shop = () => {
             maxPrice: urlParams.maxPrice
         };
         setFilter(newFilter);
-        const data = await searchProducts(urlParams.query, newFilter.category, newFilter.subcategory, newFilter.minPrice, newFilter.maxPrice, page, urlParams.sort);
+
+        updateTags(newFilter.category, newFilter.subcategory);
+
+        let data = [];
+        let currentProducts = [];
+        if (tags.length === 0) {
+            data = await searchProducts(urlParams.query, null, null, newFilter.minPrice, newFilter.maxPrice, page, urlParams.sort);
+            for (let d of data.products) {
+                let contains = false;
+                for (let t of currentProducts) {
+                    if (d.id === t.id) {
+                        contains = true;
+                    }
+                }
+                if (!contains) {
+                    currentProducts.push(d);
+                    contains = false;
+                }
+            }
+        } else {
+            for (let i = 0; i < tags.length; i++) {
+                let tagName = getCategorySubcategoryFromTag(tags[i][0]);
+                data = await searchProducts(urlParams.query, tagName.category, tagName.subcategory, newFilter.minPrice, newFilter.maxPrice, page, urlParams.sort);
+                for (let d of data.products) {
+                    let contains = false;
+                    for (let t of currentProducts) {
+                        if (d.id === t.id) {
+                            contains = true;
+                        }
+                    }
+                    if (!contains) {
+                        currentProducts.push(d);
+                        contains = false;
+                    }
+                }
+            }
+        }
+
         setFilterCount(await filterCountProducts(urlParams.query, newFilter.category, newFilter.subcategory, newFilter.minPrice, newFilter.maxPrice));
-        setProducts(data.products);
+        setProducts(currentProducts);
         setLastPage(data.lastPage);
+
         if (queryChanged && urlParams.query !== undefined && data.didYouMean !== "" && urlParams.query !== data.didYouMean) {
             showMessage("warning", (
                 <>
@@ -140,66 +222,83 @@ const Shop = () => {
         });
     }
 
-
     return (
-        <div className="shop-container">
-            <div className="shop-filters-container">
-                <CategoriesFilter filter={filter} handleClick={handleClick} query={urlParams.query}/>
-                <PriceFilter minPrice={urlParams.minPrice} maxPrice={urlParams.maxPrice} filterCount={filterCount}
-                             handleClick={handlePriceClick}/>
+        <>
+            <div style={{marginBottom: "20px", marginTop: "-20px"}}>
+                <ReactTagInput
+                    tags={tags}
+                    placeholder=" "
+                    removeOnBackspace={true}
+                    editable={false}
+                    onChange={(newTags) => {
+                        setTags(newTags);
+                    }}
+                />
             </div>
+            <div>
+                <div className="shop-container">
+                    <div className="shop-filters-container">
+                        <CategoriesFilter filter={filter} handleClick={handleClick} query={urlParams.query}/>
+                        <PriceFilter minPrice={urlParams.minPrice} maxPrice={urlParams.maxPrice}
+                                     filterCount={filterCount}
+                                     handleClick={handlePriceClick}/>
+                    </div>
 
-            <div className="shop-products-container">
-                <div className="shop-sorting-bar">
-                    <Form.Control defaultValue={urlParams.sort} onChange={e => sortBy(e.target.value)} size="lg"
-                                  as="select" className="sort-select">
-                        <option value="default">Default Sorting</option>
-                        <option value="start-date-desc">Added: New to Old</option>
-                        <option value="end-date-asc">Time Left</option>
-                        <option value="price-asc">Price: Low to High</option>
-                        <option value="price-desc">Price: High to Low</option>
-                    </Form.Control>
-                    {loading ? <Spinner className="shop-spinner" animation="border"/> : null}
-                    <div style={{display: 'flex'}}>
-                        <Button onClick={() => setGridLayout(true)}
-                                style={gridLayout ? {color: 'white', backgroundColor: '#8367D8'} : null} size="lg"
-                                variant="transparent">
-                            <BsGrid3X3GapFill style={{marginRight: 10}}/>
-                            Grid
-                        </Button>
-                        <Button onClick={() => setGridLayout(false)}
-                                style={gridLayout ? null : {color: 'white', backgroundColor: '#8367D8'}} size="lg"
-                                variant="transparent">
-                            <FaThList style={{marginRight: 10}}/>
-                            List
-                        </Button>
+                    <div className="shop-products-container">
+                        <div className="shop-sorting-bar">
+                            <Form.Control defaultValue={urlParams.sort} onChange={e => sortBy(e.target.value)} size="lg"
+                                          as="select" className="sort-select">
+                                <option value="default">Default Sorting</option>
+                                <option value="start-date-desc">Added: New to Old</option>
+                                <option value="end-date-asc">Time Left</option>
+                                <option value="price-asc">Price: Low to High</option>
+                                <option value="price-desc">Price: High to Low</option>
+                            </Form.Control>
+                            {loading ? <Spinner className="shop-spinner" animation="border"/> : null}
+                            <div style={{display: 'flex'}}>
+                                <Button onClick={() => setGridLayout(true)}
+                                        style={gridLayout ? {color: 'white', backgroundColor: '#8367D8'} : null}
+                                        size="lg"
+                                        variant="transparent">
+                                    <BsGrid3X3GapFill style={{marginRight: 10}}/>
+                                    Grid
+                                </Button>
+                                <Button onClick={() => setGridLayout(false)}
+                                        style={gridLayout ? null : {color: 'white', backgroundColor: '#8367D8'}}
+                                        size="lg"
+                                        variant="transparent">
+                                    <FaThList style={{marginRight: 10}}/>
+                                    List
+                                </Button>
+                            </div>
+                        </div>
+
+                        <div style={!gridLayout ? {display: 'unset'} : null} className="shop-products">
+                            {products.map(product => gridLayout ? (
+                                <ImageCardOverlay key={product.id} data={product} url={productUrl(product)}>
+                                    <ImageCard key={product.id} data={product} size="xl" url={productUrl(product)}/>
+                                </ImageCardOverlay>
+                            ) : (
+                                <ListCard key={product.id} data={product} url={productUrl(product)}/>
+                            ))}
+                        </div>
+
+                        {!loading && products.length === 0 ? <ItemNotFound/> : null}
+
+                        {!lastPage ?
+                            <div style={{width: '100%', marginTop: 50}}>
+                                <Button disabled={loading}
+                                        onClick={exploreMore}
+                                        style={{width: 250, margin: '0 auto'}}
+                                        variant="fill-purple"
+                                        size="xxl">
+                                    EXPLORE MORE
+                                </Button>
+                            </div> : null}
                     </div>
                 </div>
-
-                <div style={!gridLayout ? {display: 'unset'} : null} className="shop-products">
-                    {products.map(product => gridLayout ? (
-                        <ImageCardOverlay key={product.id} data={product} url={productUrl(product)}>
-                            <ImageCard key={product.id} data={product} size="xl" url={productUrl(product)}/>
-                        </ImageCardOverlay>
-                    ) : (
-                        <ListCard key={product.id} data={product} url={productUrl(product)}/>
-                    ))}
-                </div>
-
-                {!loading && products.length === 0 ? <ItemNotFound/> : null}
-
-                {!lastPage ?
-                    <div style={{width: '100%', marginTop: 50}}>
-                        <Button disabled={loading}
-                                onClick={exploreMore}
-                                style={{width: 250, margin: '0 auto'}}
-                                variant="fill-purple"
-                                size="xxl">
-                            EXPLORE MORE
-                        </Button>
-                    </div> : null}
             </div>
-        </div>
+        </>
     );
 }
 
