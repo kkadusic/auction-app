bdeb21a67c3aaa016570fa6354ea93e54bab582e
kkadusic <kkadusic1@etf.unsa.ba>
2021-04-27 16:40:13 +0200

    Update error handling

--- backend/src/main/java/com/atlantbh/auctionapp/exception/RestExceptionHandler.java
+++ backend/src/main/java/com/atlantbh/auctionapp/exception/RestExceptionHandler.java
@@ -4,6 +4,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
@@ -13,10 +16,31 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        CustomExceptionResponse body = getDefaultExceptionBody(status, request);
+        body.setMessage(ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        CustomExceptionResponse body = getDefaultExceptionBody(status, request);
+        body.setMessage(ex.getMessage());
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         CustomExceptionResponse body = getDefaultExceptionBody(status, request);
         body.setMessage("JSON parse error");
         return new ResponseEntity<>(body, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        CustomExceptionResponse body = getDefaultExceptionBody(status, request);
+        body.setMessage(ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage());
+        return super.handleBindException(ex, headers, status, request);
     }
 
     private CustomExceptionResponse getDefaultExceptionBody(HttpStatus status, WebRequest request) {
