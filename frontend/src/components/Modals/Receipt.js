cd716a9a526d86206c80b3229622944e38e5931d
kkadusic <kkadusic1@etf.unsa.ba>
2021-04-28 21:47:23 +0200

    Change payment receipt UI

--- frontend/src/components/Modals/Receipt.js
+++ frontend/src/components/Modals/Receipt.js
@@ -23,16 +23,16 @@ const Receipt = ({showModal, setShowModal, productId}) => {
     }, [productId]);
 
     return (
-        <Modal size="xl" centered show={showModal} onHide={() => setShowModal(false)}>
-            <div style={{margin: '0 auto', width: '100%'}}>
+        <Modal size="lg" centered show={showModal} onHide={() => setShowModal(false)}>
+            <div style={{margin: '0 auto', width: '80%'}}>
                 <div className="tab-title">
-                    RECEIPT
+                    PAYMENT RECEIPT
                 </div>
                 <div className="tab-content">
                     <div className="receipt-part-title">
                         Transaction information
                     </div>
-                    <div className="gray-line"/>
+                    <div className="grey-line"/>
                     <div className="receipt-part">
                         <span>
                             <span className="receipt-label">
@@ -40,12 +40,14 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                             </span>
                             {receipt.id}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Date:
                             </span>
                             {moment.utc(receipt.date).local().format(longDateTimeFormat)}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Amount:
@@ -57,7 +59,7 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                     <div className="receipt-part-title">
                         Product information
                     </div>
-                    <div className="gray-line"/>
+                    <div className="grey-line"/>
                     <div className="receipt-part">
                         <span>
                             <span className="receipt-label">
@@ -65,24 +67,28 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                             </span>
                             {receipt.name}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Color:
                             </span>
                             {receipt.color ? receipt.color : "/"}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Size:
                             </span>
                             {receipt.size ? receipt.size : "/"}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Shipping:
                             </span>
                             {receipt.shipping ? "Yes" : "No"}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Description:
@@ -94,7 +100,7 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                     <div className="receipt-part-title">
                         Seller information
                     </div>
-                    <div className="gray-line"/>
+                    <div className="grey-line"/>
                     <div className="receipt-part">
                         <span>
                             <span className="receipt-label">
@@ -102,30 +108,35 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                             </span>
                             {receipt.sellerName}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Street:
                             </span>
                             {receipt.sellerStreet}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Country:
                             </span>
                             {receipt.sellerCountry}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 City:
                             </span>
                             {receipt.sellerCity}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Zip:
                             </span>
                             {receipt.sellerZip}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Phone:
@@ -137,7 +148,7 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                     <div className="receipt-part-title">
                         Shipping information
                     </div>
-                    <div className="gray-line"/>
+                    <div className="grey-line"/>
                     <div className="receipt-part">
                         <span>
                             <span className="receipt-label">
@@ -145,24 +156,28 @@ const Receipt = ({showModal, setShowModal, productId}) => {
                             </span>
                             {receipt.street}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Country:
                             </span>
                             {receipt.country}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 City:
                             </span>
                             {receipt.city}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Zip:
                             </span>
                             {receipt.zip}
                         </span>
+                        <br/>
                         <span>
                             <span className="receipt-label">
                                 Phone:
